26ccfdb796659f7504790752dbbfdca298d205e0
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-05-13 13:44:48 -0700

    working so far

--- faidx.py
+++ faidx.py
@@ -15,9 +15,9 @@ def get_file_paths(path):
         sys.exit(-1)
     faidx_path = fasta_path.with_suffix(fasta_path.suffix + ".fai")
     if faidx_path.exists():
-        sys.stderr.write("path '{}' already exists, nothing to do\n".format(str(faidx_path)))
+        sys.stderr.write("path '{}' already exists, overwriting\n".format(str(faidx_path)))
         sys.stderr.flush()
-        sys.exit(0)
+        faidx_path.unlink()
     return fasta_path, faidx_path
 
 def main(argc, argv):
@@ -28,6 +28,32 @@ def main(argc, argv):
         return -1
 
     fasta_path, faidx_path = get_file_paths(argv[1])
+
+    fasta_handler = open(str(fasta_path), "r")
+    faidx_handler = open(str(faidx_path), "w")
+
+    name = ""
+    fpos = foffset = readlen = linelen = 0
+
+    for line in fasta_handler.readlines():
+        assert line[-1] == "\n"
+        if line[0] == ">":
+            if readlen > 0:
+                faidx_handler.write("{}\t{}\t{}\t{}\t{}\n".format(name, readlen, fpos, linelen-1, linelen))
+                readlen = 0
+            name = line.lstrip(">").split()[0].rstrip()
+        else:
+            if readlen == 0:
+                fpos = foffset
+                linelen = len(line)
+            readlen += len(line)-1
+        foffset += len(line)
+
+    if readlen > 0:
+        faidx_handler.write("{}\t{}\t{}\t{}\t{}\n".format(name, readlen, fpos, linelen-1, linelen))
+
+    fasta_handler.close()
+    faidx_handler.close()
 
 if __name__ == "__main__":
     sys.exit(main(len(sys.argv), sys.argv))
